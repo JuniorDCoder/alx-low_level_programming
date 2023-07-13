cf7e1dd55a95e7934b4c8c56fd6bce2499bdbe92
JuniorDCoder <foryoungjuniorngu@gmail.com>
2023-07-13 07:07:49 -0700

    Outputing to standard output using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - The entry point of the program
+ * Return: The return value of the main function. Usually 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
