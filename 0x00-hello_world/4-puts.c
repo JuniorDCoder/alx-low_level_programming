191abf2168083bfe0ac69d39ea48e0e697c89278
JuniorDCoder <foryoungjuniorngu@gmail.com>
2023-07-13 06:59:15 -0700

    Print message to the stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of the C program
+ * Return: Specifies the return value for the program. Ususally 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
