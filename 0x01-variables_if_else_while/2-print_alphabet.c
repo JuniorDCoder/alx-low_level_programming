e99c6660d2caaa53a84d42e6f55baf3c8aaca764
JuniorDCoder <foryoungjuniorngu@gmail.com>
2023-07-14 12:13:50 -0700

    New commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: return value of the main function
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
